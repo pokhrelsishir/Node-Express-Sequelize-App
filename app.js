bc4256db52fbfc0ec78cde4938ab76ebd840a3ff
Sishir Pokhrel <pokhrelsishir@gmail.com>
2020-02-16 01:59:02 -0600

    Initial Commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 
 var app = express()
 
-app.get('/', function (req, res) {
+app.get('/name', function (req, res) {
     res.json({
         name: "Sishir Pokhrel"
     })
