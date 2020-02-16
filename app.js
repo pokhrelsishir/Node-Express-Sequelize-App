0e8993131ca67415eaf2b9d8c1fbd2294aefc6a6
Sishir Pokhrel <pokhrelsishir@gmail.com>
2020-02-16 14:22:47 -0600

    Initial Commit

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
-var express = require('express')
+'use strict';
+var express = require('express');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+
+
 
 var app = express()
 
-app.get('/name', function (req, res) {
-    res.json({
-        name: "Sishir Pokhrel"
-    })
-})
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.listen(3000)
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+if (app.get('env') == 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.json({
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.join({
+        message: err.message,
+        error: {}
+
+    });
+});
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function () {
+    console.log('Express Server listining on port ' + server.address().port);
+});
+
